0ec2d5a3cfdbef2752b79986e45bc5d794c503c2
Izushi <shoeshinesamurai@gmail.com>
2024-09-26 00:02:13 -0700

    installed framer motion and demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { motion, spring } from "framer-motion";
 
 function App() {
   return (
-    <>
-      <div className="text-red-500">
-        Hello world!
-      </div>
-    </>
+    <div className="h-screen flex items-center justify-center">
+      <motion.div
+        initial={{ x: -100 }}
+        animate={{ x: 100 }}
+        transition={{
+          type: "spring",
+          duration: 2,
+          delay: 1,
+          stiffness: 200
+        }}
+        className="w-28 h-28 bg-red-600"></motion.div>
+    </div>
   )
 }
 
