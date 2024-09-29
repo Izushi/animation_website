561ef6a7835a97e5788dd8fb274bf13460852e7e
Izushi <shoeshinesamurai@gmail.com>
2024-09-28 23:27:08 -0700

    set a motion to each elements

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
       <div className="container mx-auto">
         {/* header */}
         <motion.header
-        initial={{ y: -100 }}
-        animate={{ y: 0 }}
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
         transition={{
           type: 'spring',
           duration: 1,
@@ -35,21 +35,48 @@ function App() {
         <section className="lg:py-48">
           <div className="flex items-center justify-between">
             {/* left */}
-            <div>
-              <h2 className="text-white lg:text-9xl md:text-8xl font-bold lg:max-w-[40rem]">
+            <div className="space-y-3">
+              <motion.h2
+              initial={{ y: 100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                type: 'spring',
+                duration: 1,
+                delay: 0.5,
+                stiffness: 130,
+              }}
+              className="text-white lg:text-9xl md:text-8xl font-bold lg:max-w-[40rem]">
                 Framer 3D Website
-              </h2>
-              <p className="text-white lg:max-w-[40rem] lg:text-5xl font-semibold">
+              </motion.h2>
+              <motion.p
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                type: 'spring',
+                duration: 1,
+                delay: 0.8,
+                stiffness: 130,
+              }}
+              className="text-white lg:max-w-[40rem] lg:text-5xl font-semibold">
                 The <span className="text-orange-500">next</span> generation
-              </p>
-              <div className="flex item-center gap-4">
+              </motion.p>
+              <motion.div
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                type: 'spring',
+                duration: 1,
+                delay: 0.9,
+                stiffness: 130,
+              }}
+              className="flex item-center gap-4">
                 <button className="px-6 py-3 rounded-md border-2 my-4 hover:translate-y-1 duration-150">
                   <span className="font-bold text-white">More Details</span>
                 </button>
                 <button className="px-6 py-3 rounded-md border-2 my-4  border-orange-500 hover:translate-y-1 duration-150">
                   <span className="font-bold text-orange-500">Samples</span>
                 </button>
-              </div>
+              </motion.div>
             </div>
 
             {/* 3D model */}
